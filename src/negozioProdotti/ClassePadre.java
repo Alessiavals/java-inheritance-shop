a6f74daf59b42534dcb39ced494fa63bf9ac765f
Alessiavals <alessiavalentini96@gmail.com>
2025-02-07 15:57:50 +0100

    modifica 2

---
+++ src/negozioProdotti/ClassePadre.java
@@ -0,0 +1,70 @@
+package negozioProdotti;
+
+public class ClassePadre {
+
+    private int codice;
+    private String nome;
+    private String marca;
+    private double prezzo;
+    private double iva;
+
+    public ClassePadre (int codice, String nome, String marca, double prezzo) {
+     setNome(nome);
+     setMarca(marca);
+     setPrezzo(prezzo);
+     setIva(iva);
+    }
+
+
+    // SETTER---------------
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    // GETTER------------
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    // METODI --------
+
+    public double prezzoTotale() {
+        return prezzo + (prezzo * iva / 100);
+    }
+}
--- src/negozioProdotti/UsaProdottiShop.java
+++ src/negozioProdotti/UsaProdottiShop.java
@@ -3,7 +3,7 @@ package negozioProdotti;
 public class UsaProdottiShop {
     public static void main(String[] args) {
         
-        
+        System.out.println("ciao");
 
 
     }
